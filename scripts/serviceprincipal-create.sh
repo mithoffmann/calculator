0701cce193f343cb3ddf59d640b09be8918dea0e
mithoffmann <mithoffmann@outlook.com>
2023-01-11 17:53:53 +0000

    add task deploy to azure

--- scripts/serviceprincipal-create.sh
+++ scripts/serviceprincipal-create.sh
@@ -1,1 +1,1 @@
-az ad sp create-for-rbac --role Contributor --name spCalculator --scopes /subscriptions/aa2b6366-781d-451b-bc50-598e977653c9/resourceGroups/rg-calculator --sku-auth
+az ad sp create-for-rbac --role Contributor --name spCalculator --scopes /subscriptions/aa2b6366-781d-451b-bc50-598e977653c9/resourceGroups/rg-calculator --sdk-auth
